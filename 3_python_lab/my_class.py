78b579e59447037f9df1bb6a44ee4459591929f0
jamperGG <yurastosik@gmail.com>
2024-10-22 13:20:44 +0300

    Почав працювати з INIT

---
+++ 3_python_lab/my_class.py
@@ -0,0 +1,9 @@
+class MyGroup:
+    def __init__(self, surname, name, mark):
+        
+        self.surname = surname
+        self.name = name
+        self.mark = mark
+
+def function_in_module():
+    pass
