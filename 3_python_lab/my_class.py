72cf5520ac57c141c00c5ad4642f7f46d9881fce
jamperGG <yurastosik@gmail.com>
2024-10-29 13:15:28 +0200

    попрацював з класом та попрацював з атрибутами та його основами

--- 3_python_lab/my_class.py
+++ 3_python_lab/my_class.py
@@ -1,9 +1,22 @@
-class MyGroup:
-    def __init__(self, surname, name, mark):
-        
-        self.surname = surname
-        self.name = name
-        self.mark = mark
+class Person:
+    def __init__(self, name, age):
+        self.name = name           # Публічний атрибут
+        self._age = age           # Захищений атрибут
+        self.__salary = 0         # Приватний атрибут
 
-def function_in_module():
-    pass
+    @property
+    def age(self):
+        """Властивість для доступу до захищеного атрибута"""
+        return self._age
+
+    @age.setter
+    def age(self, value):
+        """Властивість для зміни захищеного атрибута"""
+        if value < 0:
+            raise ValueError("Age cannot be negative")
+        self._age = value
+
+    @property
+    def salary(self):
+        """Властивість для доступу до приватного атрибута"""
+        return self.__salary
